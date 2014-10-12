231fdd6e333d69c69509d301b40505f950e2beeb
Jason Manion <jfjmanion@gmail.com>
2014-10-12 00:11:43 -0400

    removed files that don't exist

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 //require_once('./vendor/autoload.php');
 require_once('./vendor/JamesHeinrich/getID3/getid3/getid3.php');
 require_once('./config.php');
-require_once('./videoSeries.php');
+require_once('./videoFile.php');
+require_once('./audioFile.php');
 
 $videos = array();
 $audios = array();
@@ -13,9 +14,9 @@ $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($scannin
 foreach($objects as $name => $object){
     $pathinfo = pathinfo($name);
     if (in_array($pathinfo['extension'], $video_match) && strpos($pathinfo['filename'], 'sample') === false ){
-      $videos[] = new videoSeries($name);
+      $videos[] = new videoFile($name);
     } else if (in_array($pathinfo['extension'], $audio_match)) {
-      $audios[] = $name;
+      $audios[] = new audioFile($name);
     } else {
       $others[] = $name;
     }
@@ -24,10 +25,7 @@ foreach($objects as $name => $object){
 foreach ($videos as $video){
   //make the directory
   if ($video->is_series()) {
-    $directory = $series_dir . '/' . $video->get_series() . '/Season ' . $video->get_season() . '/';
-
-    //make the directory
-    @mkdir($directory, $permissions, true);
+    $directory = $series_dir . $video->get_series() . '/Season ' . $video->get_season() . '/';
 
     $video_location = $directory . "Episode " . $video->get_episode().".".$video->get_file_extension();
 
@@ -35,45 +33,42 @@ foreach ($videos as $video){
       echo "Series: ". $video->get_series() . "<Br>";
       echo "Season: ".$video->get_season() . "<Br>";
       echo "Episode: ". $video->get_episode() . "<Br>";
-
-      echo "Filename: ".$video->get_filename() . "<Br>";
-      echo "File Location: ".$video->get_file_location() . "<Br>";
       echo "Series Slug: ".$video->get_series_slug() . "<Br>";
+      echo "Old File Location: ".$video->get_file_location() . $video->get_filename() . "<Br>";
+      echo "New File Location: ".$video_location . "<Br><br>";
     }
   } else {
     //move it into the movies
     $directory = $movies_dir;
     $video_location = $directory . $video->get_filename();
   }
-  /* @TODO remove comment
-  if (!file_exists($video_location)){
+
+  if (!file_exists($video_location) && $prod){
+    //make the directory
+    @mkdir($directory, $permissions, true);
     copy($video->get_file_location().$video->get_filename(), $video_location );
-  }*/
+  }
 }
 
 foreach ($audios as $audio){
-  $getID3 = new getID3;
-  $ThisFileInfo = $getID3->analyze($audio);
-  getid3_lib::CopyTagsToComments($ThisFileInfo);
 
-  $audio_pathinfo = pathinfo($audio);
-  $extension = $audio_pathinfo['extension'];
-
-  $artist = $ThisFileInfo['comments']['artist'][0];
-  $year = $ThisFileInfo['comments']['year'][0];
-  $album = $ThisFileInfo['comments']['album'][0];
-  $title = $ThisFileInfo['comments']['title'][0];
-
-
+  $artist = $audio->getArtist();
   $artist = (!empty($artist)) ? $artist . "/" : "";
+  $year = $audio->getAlbumYear();
   $year = (!empty($year)) ? " (" . $year . ")" : "";
+  $album = $audio->getAlbum();
   $album = (!empty($album)) ? $album . $year . "/" : "";
 
   $directory = $music_dir . $artist . $album;
-  $filename = $title . "." . $extension;
+  $filename = $audio->getTitle() . "." . $audio->getExtension();
   $new_location = $directory . $filename;
 
-  echo "file location: " . $audio . "<br>";
+  if (!file_exists($new_location) && $prod){
+    @mkdir($directory, $permissions, true);
+    copy($audio->getFileLocation().$audio->getFileName(), $new_location);
+  }
+  if ($debug){
+  echo "old file location: " . $audio->getFileLocation(). $audio->getFileName() . "<br>";
   echo "New file location: " . $new_location . "<br><br>";
-
+  }
 }
