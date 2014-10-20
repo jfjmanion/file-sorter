74f55c67e5fe50de30daaa47930995bb1ebfbb56
Jason Manion <jfjmanion@gmail.com>
2014-10-19 22:22:29 -0400

    add sample config

---
+++ config.sample.php
@@ -0,0 +1,28 @@
+<?php
+//$flexget_config = '/home/jason/config.yml';
+$flexget_config = './config.yml';
+
+//$series_dir = '/media/seagate/Videos/Series/';
+$series_dir = './series/';
+
+$movies_dir = './movies/';
+
+$music_dir = './music/';
+
+$manual_sort = './manual_sort/';
+//$scanning_dir = '/media/seagate/Downloads/ToMove/';
+$scanning_dir = './files_to_move/';
+
+$video_match = array('mkv', 'avi', 'mp4');
+
+$audio_match = array('m4a', 'mp3', 'flac', 'wma');
+
+$permissions = 0777;
+
+$debug = true;
+
+$prod = true;
+
+$lock_file = "./lock";
+
+$ignore_files = array('txt', 'nfo', 'jpg', 'DS_Store', 'm3u', 'log');
